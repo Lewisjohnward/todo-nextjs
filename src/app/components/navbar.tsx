7de62905272bd2d2fcbd1db388220327acd38f3c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-08 15:36:30 +0100

    added demo projects

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -36,6 +36,11 @@ const navItems = [
   },
 ];
 
+const demoProjects = [
+  { title: "Welcome", tasks: 3 },
+  { title: "Try Boards", tasks: 9 },
+];
+
 const NavBar = () => {
   const [mouseEnter, setMouseEnter] = useState(false);
   const [projectsOpen, setProjectsOpen] = useState(true);
@@ -50,7 +55,7 @@ const NavBar = () => {
 
   return (
     <nav
-      className="min-w-[300px] bg-gray-50 p-5 space-y-2"
+      className="min-w-[300px] bg-gray-50 pt-8 p-6 space-y-2"
       onMouseEnter={handleMouseOver}
       onMouseLeave={handleMouseOver}
     >
@@ -63,7 +68,7 @@ const NavBar = () => {
               classNameArrow="tooltip-arrow-no-tail"
             />
             <button
-              className="flex gap-2 rounded py-1 hover:bg-gray-500/10"
+              className="flex gap-2 rounded py-2 hover:bg-gray-500/10"
               key={d.name}
               data-tooltip-id={d.tooltip.id}
               data-tooltip-content={d.tooltip.content}
@@ -76,23 +81,39 @@ const NavBar = () => {
           </>
         ))}
       </div>
-      <div className="flex justify-between items-center p-1 bg-gray-200 rounded">
-        <button className="font-semibold">Projects</button>
-        <div
-          className={clsx(mouseEnter ? "animate-fadein" : "animate-fadeout")}
-        >
-          <button className="p-2 rounded hover:bg-gray-100">
-            <AiOutlinePlus />
-          </button>
-          <button className="p-2 rounded hover:bg-gray-100">
-            <BsChevronDown
-              className={clsx(
-                "transition-transform duration-300",
-                !projectsOpen && "rotate-90"
-              )}
-              onClick={handleProjectsOpen}
-            />
-          </button>
+      <div>
+        <button className="w-full flex justify-between items-center pl-1 py-1 bg-gray-200/50 rounded">
+          <p className="font-semibold">Projects</p>
+          <div
+            className={clsx(mouseEnter ? "animate-fadein" : "animate-fadeout")}
+          >
+            <button className="p-2 rounded hover:bg-white/50">
+              <AiOutlinePlus />
+            </button>
+            <button className="p-2 rounded hover:bg-white/50">
+              <BsChevronDown
+                className={clsx(
+                  "transition-transform duration-300",
+                  !projectsOpen && "rotate-90"
+                )}
+                onClick={handleProjectsOpen}
+              />
+            </button>
+          </div>
+        </button>
+        <div className="flex flex-col">
+          {demoProjects.map((project) => (
+            <button
+              key={project.title}
+              className="flex items-center justify-between rounded p-2 hover:bg-gray-500/10"
+            >
+              <div className="flex items-center gap-3">
+                <div className="w-2 h-2 bg-gray-300 rounded-full" />
+                <p>{project.title}</p>
+              </div>
+              <p className="text-sm text-gray-400">{project.tasks}</p>
+            </button>
+          ))}
         </div>
       </div>
     </nav>
